4a88170447c43674600f2b5556c3be2aa79f938b
tam7777 <92511167+tam7777@users.noreply.github.com>
2022-10-14 01:21:28 +0900

    Create list1.cc

---
+++ lesson_one/list1.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+//declared a list type
+struct list {
+struct list *next;
+int data;
+};
+
+int main(){
+    struct list* h=new list;
+    struct list* s=h;
+
+for (int i=0; i<=100; i++){
+  //add number to list
+    s->data=i;
+  
+  //add next node
+    s->next=new list;
+  
+  //go to the next node. if the end of list next node is NULL
+    if ((s->data)==100){
+        s->next= NULL;
+    }else {
+        s=s->next;
+    }
+}
+
+//intialise s
+s=h;
+  
+// print the list
+while ((s=s->next)!=NULL){
+    cout<<s->data<<endl;
+}
+
+
+return(0);
+}

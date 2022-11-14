f0f304940bb170f2c70431b291421099dc8880ac
tam7777 <92511167+tam7777@users.noreply.github.com>
2022-11-15 01:12:27 +0900

    Update bptree.cc

--- lesson_five/bptree.cc
+++ lesson_five/bptree.cc
@@ -1,6 +1,7 @@
 #include "bptree.h"
 #include <vector>
 #include <sys/time.h>
+using namespace std;
 
 struct timeval
 cur_time(void)
@@ -113,31 +114,71 @@ alloc_leaf(NODE *parent)
 	return node;
 }
 
+TEMP *
+alloc_temp(NODE *parent)
+{
+	TEMP *temp;
+	if (!(temp = (TEMP *)calloc(1, sizeof(TEMP)))) ERR;
+	temp->nkey = 0;
+
+	return temp;
+}
+
+NODE *
+alloc_root(NODE *parent)
+{
+	NODE *node;
+	if (!(node = (NODE *)calloc(1, sizeof(NODE)))) ERR;
+	node->isLeaf = false;
+	node->parent = parent;
+	node->nkey = 0;
+
+	return node;
+}
 
 NODE *
 insert_in_parent(NODE *leaf,int key, NODE *fleaf)
 {   
 	int i;
-	if(leaf==Root){//if leaf is the root, leaf split
+	if(leaf==Root){//if leaf is the root	
 		NODE *root;//create new root candidate
+		root=alloc_root(NULL);
 		root->key[0]=key;//root shold contain N,K,N
 		root->chi[0]=leaf;
+		leaf->parent=root;
 		root->chi[1]=fleaf;
+		fleaf->parent=fleaf;
 		root->nkey++;
 		Root =root;//make root the Root and return 
 		return root;
 	}
+	
 	NODE *p;
-	p=leaf->parent;//let p=parent(N)
-	if(p->chi[N-1]==NULL){//if p has less then n pointers, if p has the space to put key&ptr
+	p=alloc_root(NULL);
+	leaf->parent=p;//let p=parent(N)
+	for (int i=0; i<Root->nkey;i++){
+		p->key[i]=Root->key[i];
+		p->chi[i]=Root->chi[i];
+		p->nkey++;
+	}
+
+	cout<<p->nkey<<endl;
+	if (p->chi[0]==NULL){
+		cout<<"hi"<<endl;
+	}
+/*
+	if(p->nkey>=(N-2)){//if p has less then n pointers, if p has the space to put key&ptr
 		for(int i=0; i<p->nkey; i++){// insert (K', N') to p
 			if(p->chi[i]==leaf) break;
 		}
 		p->chi[i+1]=fleaf;
 		p->key[i]=key;
 		p->nkey++;
-	}else{
+	}
+	
+	else{
 		TEMP *mem;
+		mem=alloc_temp(NULL);
 		for (i=0; i<p->nkey; i++){
 			mem->key[i]=p->key[i];
 			mem->chi[i]=p->chi[i];
@@ -158,6 +199,7 @@ insert_in_parent(NODE *leaf,int key, NODE *fleaf)
 		p->nkey=0;
 
 		NODE *fp;
+		fp=alloc_root(NULL);
 		for(i=0; i<(mem->nkey+1)/2; i++){//copt TP[n+1/2]to T
 			p->key[i]=mem->key[i];
 			p->chi[i]=mem->chi[i];
@@ -170,17 +212,19 @@ insert_in_parent(NODE *leaf,int key, NODE *fleaf)
 			fp->key[i]=mem->key[i];
 			fp->chi[i]=mem->chi[i];
 			fp->nkey++;
-		}	
+		}
+	
 		insert_in_parent(p,ffkey,fp);
+		
 	}
+	*/
 	return 0;
 }
 
 void 
 insert(int key, DATA *data)
 {
-	NODE *leaf;//create a node called leaf
-
+	NODE *leaf;//define a node called leaf
 	if (Root == NULL) {//if this is the first node
 		leaf = alloc_leaf(NULL);//parent node is NULL but is leaf and nkey=0
 		Root = leaf;//this leaf is the first node
@@ -194,39 +238,48 @@ insert(int key, DATA *data)
 		insert_in_leaf(leaf, key, data);//insert
 	}
 	else {//if the node is full 
+
 		NODE *fleaf;//create node L'
 		fleaf=alloc_leaf(NULL);
-
-		TEMP* t;//create a temporary memory that can hold n pairs
+		
+		TEMP* temp;//create a temporary memory that can hold n pairs
+		temp=alloc_temp(NULL);
+		
 		for(int i=0; i<leaf->nkey; i++){
-			t->key[i]=leaf->key[i];
-			t->chi[i]=leaf->chi[i];
-		}
-		t->nkey=leaf->nkey+1;//copy leaf to t
+			temp->key[i]=leaf->key[i];
+			temp->chi[i]=leaf->chi[i];
+			temp->nkey++;
+		}	
+		
+		insert_in_t(temp,key,data);//insert new ket & ptr to t
 
-		insert_in_t(t,key,data);//insert new ket & ptr to t
 		fleaf->chi[N-1]=leaf->chi[N-1];//set L'Pn=LPn
 		leaf->chi[N-1]=fleaf;//set LPn=L'
-
-		for(int i=0; i<leaf->nkey; i++){//erase L the key and ptr
+		
+		for(int i=0; i<leaf->nkey; i++){//erase L
 			leaf->key[i]=0;
 			leaf->chi[i]=NULL;
 		}
 		leaf->nkey=0;
 
-		for(int i=0; i<(t->nkey)/2; i++){//copy TP1 to TK[n/2] to L
-			leaf->key[i]=t->key[i];
-			leaf->chi[i]=t->chi[i];
+		for(int i=0; i<(temp->nkey)/2; i++){//copy TP1 to TK[n/2] to L
+			leaf->key[i]=temp->key[i];
+			leaf->chi[i]=temp->chi[i];
 			leaf->nkey++;
 		}
 
-		for(int i=(t->nkey)/2; i<t->nkey; i++){//copy TP[n/2] to TKn to L'
-			fleaf->key[i]=t->key[i];
-			fleaf->chi[i]=t->chi[i];
-			leaf->nkey++;
+		int j=0;
+
+		for(int i=(temp->nkey)/2; i<temp->nkey; i++){//copy TP[n/2] to TKn to L'
+			fleaf->key[j]=temp->key[i];
+			fleaf->chi[j]=temp->chi[i];
+			j++;
+			fleaf->nkey++;
 		}
 		int fkey=fleaf->key[0];
+
 		insert_in_parent(leaf,fkey,fleaf);
+		
 	}
 }
 
@@ -267,3 +320,4 @@ main(int argc, char *argv[])
 
 //when inserting a num to t, using insert_in_leaf, can not do it coz
 //cannot 'TEMP'to 'NODE' so created a insert function just for TEMP
+//what do we need alloc_leaf?

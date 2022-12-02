e8049330cd4030e1c548f12c636d3ff0a3e28a50
tam7777 <92511167+tam7777@users.noreply.github.com>
2022-12-02 16:02:01 +0900

    Create counter_pthread_lock.cc

---
+++ counter/counter_pthread_lock.cc
@@ -0,0 +1,32 @@
+#include "lock.h"
+uint Counter = 0;
+
+
+void *
+worker(void *arg)
+{
+  for (uint i = 0; i < NbLoop; i++) {
+    if((pthread_mutex_lock(&Lock))==-1) ERR;
+    Counter++;
+    if((pthread_mutex_unlock(&Lock))==-1) ERR;    
+  }
+
+  return NULL;
+} 
+
+int
+main(void)
+{
+  pthread_t thread[NbThread];
+  if ((pthread_mutex_init(&Lock, NULL)) == -1) ERR;
+
+  for (uint i = 0; i < NbThread; i++) {
+    pthread_create(&thread[i], NULL, worker, NULL);
+  }
+  for (uint i = 0; i < NbThread; i++) {
+    pthread_join(thread[i], NULL);
+  }
+  printf("Counter: %u (Ref. %u)\n", Counter, NbThread * NbLoop);
+
+  return 0;
+}
